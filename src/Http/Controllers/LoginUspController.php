7985445fb5c5f2d54c7f839f116aa8552420a942
Guilherme Frias <guilherme.frias.o.t3@gmail.com>
2026-03-10 16:18:26 -0300

    [FEAT]: Login/SignUp funcionando com ressalvas estruturais

--- src/Http/Controllers/LoginUspController.php
+++ src/Http/Controllers/LoginUspController.php
@@ -9,24 +9,65 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Profile;
+use Purify;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
 
 
 class LoginUspController extends Controller
-{
+{   
+    use RegistersUsers;
+    
+    protected $redirectTo = '/i/web';
+
     public function index()
     {
         return view('senhaunicapixelfed::loginusp');
     }
 
+    public function create(array $data)
+    {
+        // if (config('database.default') == 'pgsql') {
+        //     $data['username'] = strtolower($data['username']);
+        //     $data['email'] = strtolower($data['email']);
+        // }
+
+        return User::create([
+            'name' => Purify::clean($data['name']),
+            'username' => $data['username'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'app_register_ip' => request()->ip(),
+        ]);
+    }
+
+
     public function store(Request $request) {
-        $user = User::firstOrcreate(
-            ['email' => $request->email],
-            ['password' => Hash::make(Str::random(40))]
-        );
+        // dd($request->all() + ['password' => Hash::make(Str::random(40))]);
+        // $user = User::firstOrCreate(
+        //     ['email' => $request->email],
+        //     ['password' => Hash::make(Str::random(40))]
+        // );
+
+        event(new Registered($user = $this->create($request->all()
+            + [
+                'password' => Str::random(40),
+                'name' => Str::random(10),
+                'username' => Str::random(5)
+            ]
+        )));
+
+        $this->guard()->login($user);
+        // dd($user);
+         
+        // Auth::loginUsingId($user->id);
+        // $request->session()->regenerate();
+
+        return $this->registered($request, $user)
+            ?: redirect($this->redirectPath());
         
-        Auth::login($user);
-        $request->session()->regenerate();
-        
-        return redirect('/@');
+        // return redirect('/home');
     }
 }
